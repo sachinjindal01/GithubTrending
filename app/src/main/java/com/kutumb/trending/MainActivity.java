5f9bae215fab2f09ba0212e6a66a1521d3b957e6
Sachin Jindal <jindalsachin01@gmail.com>
2022-01-25 16:42:18 +0530

    Add SwipeRefereshLayout and data binding.
    This allows activity to implement the SwipeLayout and add it in xml layout file as well to refresh from internet
    
    This also adds data binding in gradle , ayout and activity file.

--- app/src/main/java/com/kutumb/trending/MainActivity.java
+++ app/src/main/java/com/kutumb/trending/MainActivity.java
@@ -1,14 +1,26 @@
 package com.kutumb.trending;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.kutumb.trending.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+   private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    @Override
+    public void onRefresh() {
+
     }
 }
